db34c949952135ce658e4d64ad2a5ab1da1e7215
Michael Williams <dinosaur@riseup.net>
2014-09-22 15:43:29 -0700

    update isEntitySchema to actually check schema, plus work for both input and output of EntitySchema constructor

--- isEntitySchema.js
+++ isEntitySchema.js
@@ -1,10 +1,15 @@
 var debug = require('debug')('entity-schema:isEntitySchema');
 
-module.exports = function isEntitySchema (obj) {
-  debug(obj);
+module.exports = function isEntitySchema (schema) {
+  debug(schema);
+
+  if (schema && schema.schema) {
+    return isEntitySchema(schema.schema);
+  }
 
   return !!(
-    typeof obj === 'object' &&
-    typeof obj.schema === 'object'
+    typeof schema === 'object' &&
+    typeof schema.id === 'string' &&
+    typeof schema.properties === 'object'
   );
 };
